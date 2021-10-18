6ac9e082f656cf0fe0b1d15c6d976a30ad239a33
Yam Ebgui <yam.ebgui@gmail.com>
2021-10-18 12:40:14 +0300

    add comments to index.js, fix the structure of the html
    I have changed the structure of the html to fit the web design that I wanted.
    also remove some bootstrap classes that were not in use.

--- src/index.js
+++ src/index.js
@@ -7,11 +7,12 @@ const pockemonImage = document.getElementById("pockemonImage");
 const pockemonTypes = document.getElementById("pockemonTypes");
 const pockemonFamily = document.getElementById("pockemonFamily");
 
-
+//variable section
 let frontImageURL = "";
 let backImageURL = "";
 const baseURL = 'https://pokeapi.co/api/v2/'
 
+//create element in the Dom with the arguments that it get
 function createElement(tagName, children = [], classes = [], attributes = {}) {
     let newElement = document.createElement(tagName);
     newElement.append(...children);
@@ -22,37 +23,45 @@ function createElement(tagName, children = [], classes = [], attributes = {}) {
     return newElement;
 }
 
+// create li (with eventlistener of click) for each pokemon name from the data. 
 const showFamily = (data) =>{
+    document.getElementById("pockFamilyTitle").removeAttribute("hidden");
     for (let i = 0 ; i < data.length ; i++){  
-        let pockemonElement = createElement("li" , data[i].pokemon.name , ["list-group-item"]);
+        let pockemonElement = createElement("li" , data[i].pokemon.name.toUpperCase() , ["list-group-item"]);
         pockemonElement.addEventListener("click" , handlerForFamilyClick)
         pockemonFamily.append(pockemonElement);
     }
 }
 
+//create li (with eventlistener of click) for each pokemon type from the data. 
 const showTypes = (data) =>{
+    document.getElementById("pockTypesTitle").removeAttribute("hidden");
     for (let i = 0 ; i < data.length ; i++){  
-        let typeElement = createElement("li" , data[i].type.name , ["list-group-item"]);
+        let typeElement = createElement("li" , data[i].type.name.toUpperCase() , ["list-group-item"]);
         typeElement.addEventListener("click" , getFamilyFromApi)
         pockemonTypes.append(typeElement);
     }
 }
+
+// change the image to the image from the data(hover with mouse put other image from the data)
 const showImage = (data) =>{
     frontImageURL= data['sprites']['front_default'];
     backImageURL= data['sprites']['back_default'];
     pockemonImage.src = `${frontImageURL}`;
     pockemonImage.addEventListener("mouseover", handlerForHover);
 }
+
+//change DOM objecct to show the data (pockemon data)
 const showData = (data) =>{
-    pockemonName.textContent = data.name;
-    pockemonId.textContent = data.id;
-    pockemonHeight.textContent = data.height;
-    pockemonWeight.textContent = data.weight;
-    console.log(data.types)
+    pockemonName.textContent ="NAME: "+ data.name.toUpperCase();
+    pockemonId.textContent = "ID: "+  data.id;
+    pockemonHeight.textContent ="HEIGHT: " + data.height;
+    pockemonWeight.textContent ="WEIGHT " +  data.weight;
     showImage(data)
     showTypes(data.types)
 }
 
+//remove li elements from the types list in the DOM
 const cleanTypesData = () =>{
     const ulLength = pockemonTypes.childNodes.length;
     for (let i = 0 ; i < ulLength ; i++){
@@ -60,6 +69,7 @@ const cleanTypesData = () =>{
     }
 }
 
+//remove li elements from the family list in the DOM
 const cleanFamilyData = () =>{
     const ulLength = pockemonFamily.childNodes.length;
     for (let i = 0 ; i < ulLength ; i++){
@@ -67,10 +77,12 @@ const cleanFamilyData = () =>{
     }
 }
 
+//make the input area blank
 const deleteInput = (inputElement) =>{
     inputElement.value = "";
 }
 
+//send get reequest and return data from the API based on the url the user insert
 const getForApi = async (input) =>{
     try{
         let response = await axios.get(`${baseURL}pokemon/${input}`);
@@ -83,15 +95,15 @@ const getForApi = async (input) =>{
     
 }
 
-const handlerHoverOut =() =>{
-    pockemonImage.src = `${frontImageURL}`
-}
-
-//handler function => sent get request for the API and send the data to other function that show it on the page. 
+// set image src and change it back when mouse is not in the image 
 const handlerForHover = () =>{
     pockemonImage.src = `${backImageURL}`
-    pockemonImage.addEventListener("mouseout", handlerHoverOut)
+    pockemonImage.addEventListener("mouseout", ()=>{
+        pockemonImage.src = `${frontImageURL}`
+    })
 }
+
+// delete data from the DOM and update it from the data the user insert
 const handlerForSerch = async (event) =>{
     let input = document.querySelector('input').value;
     deleteInput(document.querySelector('input'));
@@ -101,16 +113,20 @@ const handlerForSerch = async (event) =>{
     showData(data);
 }
 
+//handler for click on family member that clean the DOM and show the target data.
 const handlerForFamilyClick = async (event) =>{
-    let input = event.target.textContent;
+    let input = event.target.textContent.toLowerCase();
     cleanTypesData()
     cleanFamilyData()
     let data = await getForApi(input);
     showData(data);
+    document.getElementById("pockTypesTitle").setAttribute("hidden", true);
 }
 
+// handler for click on pockemon type 
+// send get request to API and update DOM. 
 const getFamilyFromApi = async (event) =>{
-    let pockemonType = event.target.textContent;
+    let pockemonType = event.target.textContent.toLowerCase();
     console.log(`${baseURL}type/${pockemonType}/`)
     let response = await fetch (`${baseURL}type/${pockemonType}/`, {
         method: 'get',
@@ -127,6 +143,5 @@ const getFamilyFromApi = async (event) =>{
         error(`Something went wrong :${response.status} `)
     }
 }
-console.log(document.querySelector('button'))
-console.log(document.querySelector('input').value)
+
 document.querySelector('button').addEventListener("click",handlerForSerch)
